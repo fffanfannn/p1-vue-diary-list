4ae4f87ae070906912d4be429910d479f9c73590
fffanfannn <2295467@edu.vaniercollege.qc.ca>
2023-08-08 23:57:18 -0400

    add input preview

--- src/components/DiaryInput.vue
+++ src/components/DiaryInput.vue
@@ -2,19 +2,43 @@
   <hr />
   <div>
     <h2>Component: DiaryInput</h2>
-    <h3>Preview: Diary title</h3>
-    <p>Preview: Diary paragraph</p>
+    <h3>{{ titleText }}</h3>
+    <p>{{ paragraphText }}</p>
 
-    <label for="">Title: </label><input type="text" />
+    <label for="">Title: </label>
+    <input type="text" v-model="titleText" />
     <label for="">Content: </label
-    ><textarea name="" id="" cols="30" rows="10"></textarea>
-    <button>Submit</button>
+    ><textarea
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+      v-model="paragraphText"
+    ></textarea>
+    <button @click="submitBtn">Submit</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "DiaryInput",
+  data() {
+    return {
+      titleText: "",
+      paragraphText: "",
+    };
+  },
+
+  methods: {
+    submitBtn() {
+      this.$emit("submit", {
+        title: this.titleText,
+        content: this.paragraphText,
+      });
+      this.titleText = "";
+      this.paragraphText = "";
+    },
+  },
 };
 </script>
 <style></style>
