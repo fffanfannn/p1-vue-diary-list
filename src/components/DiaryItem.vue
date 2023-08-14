4a9bd54d83364fb87bfdeaf632aa2160a55e0eaf
fffanfannn <2295467@edu.vaniercollege.qc.ca>
2023-08-13 21:40:12 -0400

    item style

--- src/components/DiaryItem.vue
+++ src/components/DiaryItem.vue
@@ -1,20 +1,20 @@
 <template>
-  <li>
-    <h3>Component: DiaryItem</h3>
-    <h4>{{ eachDiaryObj.diaryTitle }}</h4>
-    <p>{{ eachDiaryObj.diaryContent }}</p>
-
-    Memories:
-    <div class="clnmemories">
-      <p v-for="(tag, index) in eachDiaryObj.diaryMemory" :key="index">
-        {{ tag }}
-      </p>
+  <div class="diaryCard">
+    <div class="diaryContent">
+      <h4>{{ eachDiaryObj.diaryTitle }}</h4>
+      <p>{{ eachDiaryObj.diaryContent }}</p>
+      <ul>
+        <li v-for="(tag, index) in eachDiaryObj.diaryMemory" :key="index">
+          {{ tag }}
+        </li>
+      </ul>
     </div>
-
     <!-- <p>Memory: {{ eachDiaryObj.diaryMemory }}</p> -->
-    <p>{{ daysCalculation }}</p>
-    <button @click="deleteBtn">Delete</button>
-  </li>
+    <div class="diaryDate">
+      <p>{{ daysCalculation }}</p>
+      <button @click="deleteBtn">Delete</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -61,22 +61,66 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-li {
-  width: 300px;
-  padding: 1rem;
-  flex-wrap: wrap;
-  list-style: none;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+.diaryCard {
+  padding: 1rem 2rem;
+  border: 1px solid rgba(0, 140, 255, 0.637);
+  text-align: left;
+  width: 90%;
+  margin: 1rem auto;
+  border-radius: 10px;
 
-.clnmemories {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 0.5rem;
-}
-.clnmemories p {
-  padding: 5px 10px;
-  border: 1px solid rgb(0, 140, 255);
-  border-radius: 15px;
+  .diaryContent {
+    h4 {
+      font-size: 1.2rem;
+      font-weight: bold;
+      margin-bottom: 1rem;
+    }
+
+    p {
+      margin-bottom: 1rem;
+    }
+
+    ul {
+      list-style: none;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: start;
+      align-items: center;
+      gap: 0.5rem;
+
+      li {
+        padding: 5px 10px;
+        border: 1px solid rgb(255 186 0);
+        border-radius: 15px;
+        font-size: 0.8rem;
+      }
+    }
+  }
+
+  .diaryDate {
+    margin-top: 1.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      border: none;
+      padding: 7px 15px;
+      font-size: 1rem;
+      transition: 1s;
+      border-radius: 10px;
+      background-color: #cae8ff;
+
+      &:hover {
+        background-color: rgb(255 186 0);
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
